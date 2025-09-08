063430b1e1a1d9fa2f36da158cf56222423d7288
blackzeshi <sergey_zhuzhgov@mail.ru>
2025-09-08 15:56:12 +0500

    fin but replay notifications

--- cmd/main.go
+++ cmd/main.go
@@ -18,18 +18,16 @@ import (
 	"github.com/zeshi09/go_web_parser_agent/internal/storage"
 )
 
+// пока храним курсоры для отслеживания изменений в базе данных в виде .json файлов
 const (
 	DomainStateFile = "domain_cursor.json"
 	LinkStateFile   = "link_cursor.json"
 )
 
-// type ForMM struct {
-// 	Id     int    `json:"id"`
-// 	Domain string `json:"domain"`
-// }
-
+// structure для курсора
 var Curs storage.Cursor
 
+// функции загрузки и сохранения данных в курсор .json
 func loadCursor(filename string) (storage.Cursor, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
@@ -49,7 +47,6 @@ func loadCursor(filename string) (storage.Cursor, error) {
 
 	return cursor, nil
 }
-
 func saveCursor(cursor storage.Cursor, filename string) error {
 	data, err := json.Marshal(cursor)
 	if err != nil {
@@ -58,6 +55,7 @@ func saveCursor(cursor storage.Cursor, filename string) error {
 	return os.WriteFile(filename, data, 0644)
 }
 
+// основной луп для работы с доменами в таблице
 func RunLoopDomain(ctx context.Context, client *ent.Client, webhook string, interval time.Duration, sof bool) error {
 	cur, err := loadCursor(DomainStateFile)
 	if err != nil {
@@ -90,12 +88,15 @@ func RunLoopDomain(ctx context.Context, client *ent.Client, webhook string, inte
 	}
 }
 
+// основной луп для работы с линками в таблице
 func RunLoopLink(ctx context.Context, client *ent.Client, webhook string, interval time.Duration, sof bool) error {
+	// загружаем курсор, чтобы просмотреть состояние изменений
 	cur, err := loadCursor(LinkStateFile)
 	if err != nil {
 		return fmt.Errorf("failed to load link cursor: %w", err)
 	}
 
+	// сканируем таблицу и вызываем notify, если что-то изменилось
 	if err := agent.ScanAndNotifyLinks(ctx, client, &cur, webhook, sof); err != nil {
 		return fmt.Errorf("initial link scan failed: %w", err)
 	}
@@ -103,9 +104,13 @@ func RunLoopLink(ctx context.Context, client *ent.Client, webhook string, interv
 		return fmt.Errorf("failed to save link cursor: %w", err)
 	}
 
+	log.Debug().Msg("мы в initial scan")
+
+	// заводим тикер для лупа
 	t := time.NewTicker(interval)
 	defer t.Stop()
 
+	// основной цикл
 	for {
 		select {
 		case <-ctx.Done():
@@ -118,37 +123,46 @@ func RunLoopLink(ctx context.Context, client *ent.Client, webhook string, interv
 			if err := saveCursor(cur, LinkStateFile); err != nil {
 				log.Error().Err(err).Msg("save cursor failed")
 			}
+			log.Debug().Msg("мы в ticker scan")
 		}
 	}
 }
 
 func main() {
+	// обозначаем время в формате unix для логов
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	// подгружаем .env файл, в котором хранятся все переменные для базы и мм
 	err := godotenv.Load()
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to load .env")
 	}
 
+	// подгружаем значение webhook url для мм из .env
 	webhook := os.Getenv("MM_WEBHOOK")
 	if webhook == "" {
 		log.Fatal().Msg("MM_WEBHOOK env var is required")
 	}
 
+	// открываем базовый клиент для postgres с вызовом DSN метода для определения строки подключения из db.go
 	client, err := ent.Open("postgres", storage.LoadConfigFromEnv().DSN())
-
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to create db client")
 	}
 	defer client.Close()
 
+	// обозначаем context
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer cancel()
 
+	// обозначаем переменные для запросов
 	interval := 30 * time.Second
 	sendOnFirst := false
 
+	// канал с ошибками для корректной обработки горутин
 	errCh := make(chan error, 2)
 
+	// открываем две горутины, которые параллельно будут проверять таблицу с доменами и ссылками
 	go func() {
 		if err := RunLoopDomain(ctx, client, webhook, interval, sendOnFirst); err != nil {
 			log.Error().Err(err).Msg("loop failed")
@@ -162,6 +176,7 @@ func main() {
 		}
 	}()
 
+	// обрабатываем завершение работы горутин (вообще завершение лупа не предполагается, только если произошла ошибка)
 	select {
 	case <-ctx.Done():
 		log.Info().Msg("shutting down...")
@@ -169,5 +184,4 @@ func main() {
 		log.Error().Err(err).Msg("app err")
 		cancel()
 	}
-
 }
